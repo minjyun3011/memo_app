fe40820696dfca0028da7dbcdeeed78a2a4c72ea
minjyun3011 <grand3canyon10@gmail.com>
2023-10-29 10:31:36 +0900

    削除機能完成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -59,4 +59,12 @@ class MemoController extends Controller
 
         return redirect(route('memos.index'));
     }
+
+    public function destroy($id)
+    {
+        $memo = Memo::find($id);
+        $memo->delete();
+
+        return redirect(route("memos.index"));
+    }
 }
